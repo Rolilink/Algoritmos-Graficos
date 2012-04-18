21df64cdd56e974488aa93ec12a0527a03952d3a
Rolilink <rolando.perez.dev@hotmail.com>
2012-04-18 16:14:22 -0500

    Punto Medio

--- custom.js
+++ custom.js
@@ -47,3 +47,29 @@ function validateall(){
 	}
 }
 
+function validateallbresenham(){
+	x1= $("#x1");
+	x2= $("#x2");
+	y1= $("#y1");
+	y2= $("#y2");
+	if(validatedigits(x1) && validatedigits(x2) && validatedigits(y1) && validatedigits(y2) /*&& (x2.val() - x1.val()) != 0*/){
+		$("#submitbutton").removeClass("disabled");
+		$("#submitbutton").removeAttr("disabled","disabled");
+	}else{
+		$("#submitbutton").addClass("disabled");
+		$("#submitbutton").attr("disabled","disabled");
+	}
+}
+
+function validateallmedio(){
+	x1= $("#x1");
+	y1= $("#y1");
+	radio= $("#radio");
+	if(validatedigits(x1) && validatedigits(y1) && validatedigits(radio) && radio.val()>0){
+		$("#submitbutton").removeClass("disabled");
+		$("#submitbutton").removeAttr("disabled","disabled");
+	}else{
+		$("#submitbutton").addClass("disabled");
+		$("#submitbutton").attr("disabled","disabled");
+	}
+}
